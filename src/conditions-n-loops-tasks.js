eb7b8317561db102d32c5058e8c931b9072ca591
vladimir <vovchikbura@mail.ru>
2024-01-05 17:57:17 +0300

    feat: create seventh function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -204,8 +204,13 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    newStr += str[i];
+  }
+
+  return str === newStr;
 }
 
 /**

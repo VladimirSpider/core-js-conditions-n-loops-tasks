33ec454308e08e133b62f67553616aacbe875bae
vladimir <vovchikbura@mail.ru>
2024-01-05 17:50:56 +0300

    feat: create sixth function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -161,8 +161,35 @@ function convertToRomanNumerals(num) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  let answer = '';
+  const words = {
+    '-': 'minus',
+    '.': 'point',
+    ',': 'point',
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+  };
+
+  for (let i = 0; i < numberStr.length; i += 1) {
+    switch (numberStr.length - i) {
+      case 1:
+        answer += words[numberStr[i]];
+        break;
+      default:
+        answer += `${words[numberStr[i]]} `;
+    }
+  }
+
+  return answer;
 }
 
 /**

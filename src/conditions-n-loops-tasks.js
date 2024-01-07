467af9f283d699bf40154e617c55e6ed61ce9e10
vladimir <vovchikbura@mail.ru>
2024-01-07 19:08:44 +0300

    feat: create getIndexOf and isContainNumber functions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -227,8 +227,14 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
 /**
@@ -246,8 +252,15 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const strNum = String(num);
+  for (let i = 0; i < strNum.length; i += 1) {
+    if (strNum[i] === String(digit)) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 /**

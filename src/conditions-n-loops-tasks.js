865a8da15d083dcc888657a76fd6ca7f75f902c6
vladimir <vovchikbura@mail.ru>
2024-01-07 23:20:01 +0300

    feat: create getSpiralMatrix function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -311,8 +311,49 @@ function getBalanceIndex(arr) {
  *          [10, 9,  8,  7]
  *        ]
  */
-function getSpiralMatrix(/* size */) {
-  throw new Error('Not implemented');
+function getSpiralMatrix(size) {
+  const ourMatrix = [];
+  for (let i = 0; i < size; i += 1) {
+    const innerArr = [];
+    for (let j = 0; j < size; j += 1) {
+      innerArr[j] = 1;
+    }
+    ourMatrix[i] = innerArr;
+  }
+
+  let count = 1;
+  let startRow = 0;
+  let endRow = size - 1;
+  let startCol = 0;
+  let endCol = size - 1;
+
+  while (startCol <= endCol && startRow <= endRow) {
+    for (let i = startCol; i <= endCol; i += 1) {
+      ourMatrix[startRow][i] = count;
+      count += 1;
+    }
+    startRow += 1;
+
+    for (let i = startRow; i <= endRow; i += 1) {
+      ourMatrix[i][endCol] = count;
+      count += 1;
+    }
+    endCol -= 1;
+
+    for (let i = endCol; i >= startCol; i -= 1) {
+      ourMatrix[endRow][i] = count;
+      count += 1;
+    }
+    endRow -= 1;
+
+    for (let i = endRow; i >= startRow; i -= 1) {
+      ourMatrix[i][startCol] = count;
+      count += 1;
+    }
+    startCol += 1;
+  }
+
+  return ourMatrix;
 }
 
 /**
